13d66fca52a2fb5f049e7420dc5399dc0caa2f89
LUAN BRENDON <luan_brendonmacedo@hotmail.com>
2024-06-21 19:03:06 -0300

    criacao da loading bar

---
+++ src/app/loading-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-bar',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      loading-bar works!
+    </p>
+  `,
+  styles: ``
+})
+export class LoadingBarComponent {
+
+}
